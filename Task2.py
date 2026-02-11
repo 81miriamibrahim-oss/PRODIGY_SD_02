3f649183f444b5dc22be5d84946148a82a9062e6
81miriamibrahim-oss <81miriamibrahim@gmail.com>
2026-02-11 19:52:33 +0200

    TASK2

---
+++ Task2.py
@@ -0,0 +1,21 @@
+import random
+
+# Generate a random number between 1 and 100
+number_to_guess = random.randint(1, 100)
+
+attempts = 0
+guess = None
+
+print("Welcome to the Guessing Game!")
+print("I have selected a number between 1 and 100.")
+
+while guess != number_to_guess:
+    guess = int(input("Enter your guess: "))
+    attempts += 1
+
+    if guess < number_to_guess:
+        print("Too low! Try again.")
+    elif guess > number_to_guess:
+        print("Too high! Try again.")
+    else:
+        print(f"Congratulations! You guessed the number in {attempts} attempts.")
